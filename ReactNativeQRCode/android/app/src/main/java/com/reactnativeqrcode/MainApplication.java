ed11ecbda30fc519c81b4d36a51d078ef7dc8906
bikedawuwang <710101991@qq.com>
2019-05-21 12:03:39 +0800

    add ScanQRcode

---
+++ ReactNativeQRCode/android/app/src/main/java/com/reactnativeqrcode/MainApplication.java
@@ -0,0 +1,55 @@
+package com.reactnativeqrcode;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.reactnativecomponent.barcode.RCTCapturePackage;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  private ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new RCTCapturePackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+
+  public void setReactNativeHost(ReactNativeHost reactNativeHost) {
+    mReactNativeHost = reactNativeHost;
+  }
+ 
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
